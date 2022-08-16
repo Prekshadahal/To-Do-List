9bb07ec8fa4cd270bd5a8115262bf04905c15eec
Preksha Dahal <prekshadahal@Prekshas-MacBook-Pro.local>
2022-08-16 14:23:42 +0545

    To Do List

---
+++ To Do Lists/TableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  To Do Lists
+//
+//  Created by Preksha Dahal on 15/08/2022.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        
+    }
+    
+}
